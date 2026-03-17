1f7eecf242fc3f891c3f7d2775c347ea8cc3318d
ze-il <hoezeil20@gmail.com>
2026-03-17 14:14:02 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '35423f65-310e-4dde-85bb-fb680710645a';
+export const MODAL_CLOSE_SEC = 2.5;
